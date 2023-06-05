3afcc73d8262a59cd2b22e4abc28d7e8c6d5d0a9
gkfla0720 <54496926+gkfla0720@users.noreply.github.com>
2023-06-05 23:37:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 let myHeading = document.querySelector('h1'); myHeading.textContent = 'Hello world!';
+let myVariable;
+myVariable = 'Bob'; let myVariable = 'Bob';
+myVariable;
+myVariable = 'Steve'; 
