d49e703db60e91c24b67ffe53202fa89c2e512a2
gkfla0720 <54496926+gkfla0720@users.noreply.github.com>
2023-06-07 22:37:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ let myVariable;
 myVariable = 'Bob'; let myVariable = 'Bob';
 myVariable;
 myVariable = 'Steve'; 
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate'){
+  alert('Yay, I love chocolate ice cram!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+function multiply(num1,num2) {
+  let result = num1 * num2;
+  return result;
+}
+multiply(4,7);
+multiply(20,20);
+multiply(0.5,3);
