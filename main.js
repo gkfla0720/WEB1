158fa8e914e06e243f98a01ed944982ec790229c
gkfla0720 <54496926+gkfla0720@users.noreply.github.com>
2023-06-08 21:54:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -21,3 +21,14 @@ multiply(0.5,3);
 document.querySelector('html').onclick = function() {
    alert('Ouch! Stop poking me!'); 
 }
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+   let mySrc = myImage.getAttribute('src');
+   if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png'); 
+   } else {
+      myImage.setAttribute ('src','images/firefox-icon.png'); 
+   } 
+}
